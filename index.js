e6bac12c4019b502aa92854b3679e70a71f4b419
hermite <stivenhermite@gmail.com>
2023-03-25 21:28:58 -0400

    Creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const name = ["Guadalupe", "Ollie","Aki"]
+
+function writeCards(name) {
+   let messageName = [] 
+for(let  i= 0; i < name.length; i ++) {
+    messageName.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+}
+return messageName
+}
+writeCards()
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log (number)
+        number --
+    }
+} 
